1a9fc5d8ce9f4bdb6ce4ffe01aae700c0b709ccf
dbigal <danilo.bigal@capgemini.com>
2021-02-14 16:59:19 -0300

    Novo Report

--- SpotifyInsights/Program.cs
+++ SpotifyInsights/Program.cs
@@ -156,7 +156,7 @@ namespace SpotifyInsights
                         }
                         else
                         {
-                            Console.WriteLine("Unable to return Artist Page information");
+                            Console.WriteLine($"Unable to return Artist Page information: {url}");
                         }
                     }
                 }
